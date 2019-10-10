d8f9a8e10a8c4284028f6f1c42673a15e5c0ecae
Raal9410 <rahm9410@gmail.com>
2019-10-10 09:58:38 -0500

    Deploy

---
+++ frontend/build/precache-manifest.55c6fab889cb35e1e819eaf0bd758e31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8f723788a82343090b1082245ddff31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c57bd94cdc24e6280d56",
+    "url": "/static/css/main.7f1e9fad.chunk.css"
+  },
+  {
+    "revision": "e93c3b0248496747d1f9",
+    "url": "/static/js/2.66a3c449.chunk.js"
+  },
+  {
+    "revision": "c57bd94cdc24e6280d56",
+    "url": "/static/js/main.5052c5e8.chunk.js"
+  },
+  {
+    "revision": "5ad6dc52180fca3143ed",
+    "url": "/static/js/runtime-main.a9e8376a.js"
+  }
+]);
